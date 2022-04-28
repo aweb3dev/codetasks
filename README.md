afafdf953aa98a061f56f7d6e3f0edd1513f5472
RebZJ <rebontoz@gmail.com>
2022-04-28 23:25:41 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Format:
+
+Folders can be named whatever
+
+The correct version is just the name of whatever file (js or sol) with _correct, 
+
+and the incomplete one is same name
+
+
+Ty
---
+++ lesson1/example.js
@@ -0,0 +1,11 @@
+import Something from something
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
