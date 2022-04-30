f5572daa07dd3476f87a2c452a1c816a3bfcc612
Complexia <complexia701@gmail.com>
2022-05-01 00:25:41 +1000

    require contract

---
+++ chap4/require.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Faucet {
+    receive() external payable {
+        
+    }
+
+    function withdraw(uint withdraw_amount) public {
+        uint max_amount = 3 * (10 ** 18);
+        /*
+            insert your code here!
+        */
+    }
+}
---
+++ chap4/require_correct.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Faucet {
+    receive() external payable {
+        
+    }
+
+    function withdraw(uint withdraw_amount) public {
+        uint max_amount = 3 * (10 ** 18);
+        require(withdraw_amount <= max_amount);
+    }
+}
