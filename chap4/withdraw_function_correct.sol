e3c7b12cd1de099be780f81133afd23fabaf6611
Complexia <complexia701@gmail.com>
2022-05-01 00:07:03 +1000

    Withdraw function defined

---
+++ chap4/withdraw_function_correct.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Faucet {
+    receive() external payable {
+        
+    }
+
+    function withdraw(uint withdraw_amount) public {
+
+    }
+}
