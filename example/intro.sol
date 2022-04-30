96ec8f0633e55c443004e191333b320625c4935e
RebZJ <rebontoz@gmail.com>
2022-04-30 20:22:19 +1000

    Create intro.sol

---
+++ example/intro.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    /*
+    insert your code here!
+    */
+    
+    function get() public view returns (string memory) {
+        return start;
+    }
+}
