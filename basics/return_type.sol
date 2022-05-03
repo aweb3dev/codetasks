cf8f8e79e8b5a41cf5d111bd45cbed1f20271ebd
Complexia <complexia701@gmail.com>
2022-05-03 13:47:00 +1000

    Return type contract function

---
+++ basics/return_type.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MeFunctions {
+    function quirkyFunction() public pure returns (uint256 x, bool b) {
+        return (1, true);
+    }
+}
